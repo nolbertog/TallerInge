96c92f4ce833e0c966876f68dbd53661cacc3564
nolbertog <nolberto.en@gmail.com>
2024-06-20 12:35:14 -0400

    reparacion carrera id

--- src/controllers/ucm/carrera.controller.js
+++ src/controllers/ucm/carrera.controller.js
@@ -1,4 +1,5 @@
 const carreraService = require('../../services/ucm/carreraServices');
+const usuarioService = require("../../services/users/usuarioServices");
 
 async function crearCarrera(req, res) {
     try {
@@ -21,11 +22,12 @@ async function obtenerCarrera(req, res) {
 async function obtenerCarreraPorId(req, res) {
     const { id } = req.params;
     try {
-        const carrera = await caches.obtenerCarreraPorId(id);
+        const carrera = await carreraService.obtenerCarreraPorId(id);
         if (!carrera) {
-            return res.status(404).json({ error: 'Carrera no encontrada' });
+            res.status(404).json({ error: 'Carrera no encontrado' });
+        } else {
+            res.status(200).json(carrera);
         }
-        res.status(200).json(carrera);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
